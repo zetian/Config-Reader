952ef6440cec5aadd695d5420f98e0e3bca9819a
Zetian <zetianzzt@gmail.com>
2017-10-26 17:37:54 -0400

    basic functioning, only strings now

--- configurator.h
+++ configurator.h
@@ -28,28 +28,28 @@ namespace configurator{
         std::string s, key, value;
         
         // For each (key, value) pair in the file
-        while (std::getline( ins, s )){
+        while (std::getline(ins, s)){
             std::string::size_type begin = s.find_first_not_of( " \f\t\v" );
             
             // Skip blank lines
             if (begin == std::string::npos) continue;
     
             // Skip commentary
-            if (std::string( "#;" ).find(s[begin]) != std::string::npos) continue;
+            if (std::string("#;").find(s[begin]) != std::string::npos) continue;
     
             // Extract the key value
-            std::string::size_type end = s.find( '=', begin );
-            key = s.substr( begin, end - begin );
+            std::string::size_type end = s.find('=', begin);
+            key = s.substr(begin, end - begin);
     
             // (No leading or trailing whitespace allowed)
-            key.erase( key.find_last_not_of( " \f\t\v" ) + 1 );
+            key.erase( key.find_last_not_of(" \f\t\v" ) + 1);
     
             // No blank keys allowed
             if (key.empty()) continue;
     
             // Extract the value (no leading or trailing whitespace allowed)
-            begin = s.find_first_not_of( " \f\n\r\t\v", end + 1 );
-            end   = s.find_last_not_of( " \f\n\r\t\v" ) + 1;
+            begin = s.find_first_not_of(" \f\n\r\t\v", end + 1);
+            end   = s.find_last_not_of(" \f\n\r\t\v" ) + 1;
             value = s.substr(begin, end - begin);
     
             // Insert the properly extracted (key, value) pair into the map
