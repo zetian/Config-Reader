1121f99707d6bda40d265f266c04d27a25bd3732
Zetian Zhang <zetian@Zetians-Mac.local>
2017-10-26 22:31:35 -0400

    add comments

--- configurator.h
+++ configurator.h
@@ -32,12 +32,6 @@ private:
     //     bool iskey(const std::string& s) const{
     //         return count(s) != 0;
     //     }
-
-    //     double get_real(std::string name, double default_value){
-    //         std::string valstr = this[name];
-    //         return default_value;
-    //     }
-    // };
 public:
     std::map <std::string, std::string> GetAllData(){
         return data_;
@@ -47,8 +41,9 @@ public:
         return default_value;
     }
 
-
-
+    //---------------------------------------------------------------------------
+    // The extraction operator reads configurator until EOF.
+    // Invalid data is ignored.
     friend std::istream& operator >> (std::istream& ins, Configurator& configurator){
         std::string s, key, value;
         
@@ -83,6 +78,8 @@ public:
         return ins;
     };
 
+    //---------------------------------------------------------------------------
+    // The insertion operator writes all configurator data to stream.
     friend std::ostream& operator << (std::ostream& outs, const Configurator& configurator){
         std::map<std::string, std::string>::const_iterator iter;
         for (iter = configurator.data_.begin(); iter != configurator.data_.end(); iter++){
@@ -90,54 +87,6 @@ public:
         }
         return outs;
     };
-
-    //---------------------------------------------------------------------------
-    // The extraction operator reads configurator::data until EOF.
-    // Invalid data is ignored.
-    //
-    // std::istream& operator >> (std::istream& ins, data& d){
-    //     std::string s, key, value;
-        
-    //     // For each (key, value) pair in the file
-    //     while (std::getline(ins, s)){
-    //         std::string::size_type begin = s.find_first_not_of(" \f\t\v");
-            
-    //         // Skip blank lines
-    //         if (begin == std::string::npos) continue;
-    
-    //         // Skip commentary
-    //         if (std::string("#;").find(s[begin]) != std::string::npos) continue;
-    
-    //         // Extract the key value
-    //         std::string::size_type end = s.find('=', begin);
-    //         key = s.substr(begin, end - begin);
-    
-    //         // (No leading or trailing whitespace allowed)
-    //         key.erase(key.find_last_not_of(" \f\t\v" ) + 1);
-    
-    //         // No blank keys allowed
-    //         if (key.empty()) continue;
-    
-    //         // Extract the value (no leading or trailing whitespace allowed)
-    //         begin = s.find_first_not_of(" \f\n\r\t\v", end + 1);
-    //         end   = s.find_last_not_of(" \f\n\r\t\v") + 1;
-    //         value = s.substr(begin, end - begin);
-    
-    //         // Insert the properly extracted (key, value) pair into the map
-    //         d[key] = value;
-    //     }
-    //     return ins;
-    // }
-    //---------------------------------------------------------------------------
-    // The insertion operator writes all configurator::data to stream.
-    //
-    // std::ostream& operator << (std::ostream& outs, const data& d){
-    //     data::const_iterator iter;
-    //     for (iter = d.begin(); iter != d.end(); iter++){
-    //         std::cout << iter->first << " = " << iter->second << std::endl;
-    //     }
-    //     return outs;
-    // }
 };
 
 #endif  // __CONFIGURATOR_H__
